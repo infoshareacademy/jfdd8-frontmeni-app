0e83653bf594f67ae22d7aca091a5bddf1123cf6
Dominik Dutkiewicz <dominikdut@gmail.com>
2017-12-14 15:42:42 +0100

    NavBar example added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import NavBar from './NavBar'
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,6 +7,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+
+        <NavBar/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
---
+++ src/NavBar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Menu } from 'semantic-ui-react'
+
+class NavBar extends Component {
+  state = { activeItem: 'bio' };
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <Menu tabular>
+        <Menu.Item name='bio' active={activeItem === 'bio'} onClick={this.handleItemClick} />
+        <Menu.Item name='photos' active={activeItem === 'photos'} onClick={this.handleItemClick} />
+      </Menu>
+    )
+  }
+}
+export default NavBar;
