b5eb66c28e15a1d4739418b44b3a13ca255467ed
Marcin Wisniewski <mwgithub@o2.pl>
2017-12-14 16:43:37 +0100

    WiP calendar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BigCalendar from 'calendar'
+import moment from 'moment';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <BigCalendar/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
---
+++ src/calendar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+
+let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k])
+
+let Basic = React.createClass({
+  render(){
+    return (
+      <BigCalendar
+        {...this.props}
+        // events={events}
+        views={allViews}
+        step={60}
+        defaultDate={new Date(2015, 3, 1)}
+      />
+    )
+  }
+})
+
+export default Basic;
+export default BigCalendar
