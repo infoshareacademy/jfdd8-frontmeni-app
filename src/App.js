436265d3b3983fbcbea9b94503296166154a1ea9
Marcin Wisniewski <mwgithub@o2.pl>
2017-12-14 16:49:52 +0100

    WiP calendar, replace react.createClass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import BigCalendar from 'calendar'
+import BigCalendar from 'react-big-calendar'
 import moment from 'moment';
 
 
--- src/calendar.js
+++ src/calendar.js
@@ -2,9 +2,13 @@ import React from 'react';
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
 
+BigCalendar.setLocalizer(
+  BigCalendar.momentLocalizer(moment)
+);
+
 let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k])
 
-let Basic = React.createClass({
+({
   render(){
     return (
       <BigCalendar
