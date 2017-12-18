2922528ad64f63560966a3e54458276d34dcbfd4
zakrza59 <zakrza59@gmail.com>
2017-12-18 16:00:51 +0100

    Added flags.

--- src/ProfileCreator.js
+++ src/ProfileCreator.js
@@ -1,5 +1,13 @@
 import React from 'react'
-import { Form, Input } from 'semantic-ui-react'
+import { Form, Input, Dropdown } from 'semantic-ui-react'
+const countryOptions = [
+  { key: 'eu', value: 'eu', flag: 'eu', text: 'Europe' },
+  { key: 'us', value: 'us', flag: 'us', text: 'USA' },
+  { key: 'gb', value: 'gb', flag: 'gb', text: 'United Kingdom' },
+  { key: 'ru', value: 'ru', flag: 'ru', text: 'Россия' },
+  { key: 'jp', value: 'jp', flag: 'jp', text: '日本国' },
+  { key: 'pl', value: 'pl', flag: 'pl', text: 'Polska' }
+  ];
 
 const ProfileCreator = () => (
   <Form>
@@ -10,15 +18,20 @@ const ProfileCreator = () => (
         <option value='female'>Female</option>
         <option value='neutral'>Neutral</option>
       </Form.Field>
-      <Form.Field label='Weight' control='input' />
-      <Form.Field label='Height' control='input' />
-      <Form.Field label='Full Name' control='input' />
       <Input
         label={{ basic: true, content: 'kg' }}
         labelPosition='right'
         placeholder='Enter weight...'
       />
-
+      <Input
+        label={{ basic: true, content: 'cm' }}
+        labelPosition='right'
+        placeholder='Enter height...'
+      />
+      <Form.Field label='births' control='input' />
+      <Form.Field label='country' control='input' />
+      <Form.Field label='zip' control='input' />
+      <Dropdown placeholder='Select Country' fluid search selection options={countryOptions} />
     </Form.Group>
 
 
