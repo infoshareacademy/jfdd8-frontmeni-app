dc9fe941a83af3dee6227ebb5937fd6b2333c1e0
Dominik Dutkiewicz <dominikdut@gmail.com>
2017-12-18 15:55:59 +0100

    Added top navbar

--- src/FoodScreen.js
+++ src/FoodScreen.js
@@ -1,30 +1,36 @@
-import React, { Component } from 'react'
-
+import React, {Component} from 'react'
 
 class FoodScreen extends Component {
   render() {
     return (
-      <table className="ui red table">
-        <thead>
-        <tr><th>Food</th>
-          <th>Calories</th>
-          <th>Protein</th>
-        </tr></thead><tbody>
-      <tr>
-        <td>Apples</td>
-        <td>200</td>
-        <td>0g</td>
-      </tr>
-      <tr>
-        <td>Orange</td>
-        <td>310</td>
-        <td>0g</td>
-      </tr>
-      </tbody>
-      </table>
+      <div>
+        <table className="ui red table">
+          <thead>
+          <tr>
+            <th>Food</th>
+            <th>Calories</th>
+            <th>Protein</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>Apples</td>
+            <td>200</td>
+            <td>0g</td>
+          </tr>
+          <tr>
+            <td>Orange</td>
+            <td>310</td>
+            <td>0g</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
     )
+
   }
 }
+
 export default FoodScreen;
 
 
--- src/index.js
+++ src/index.js
@@ -13,23 +13,26 @@ import FoodScreen from "./FoodScreen";
 import ExercisesScreen from "./ExercisesScreen";
 import CalorieTableScreen from "./CalorieTableScreen";
 import SettingsScreen from "./SettingsScreen";
+import NavBar from "./NavBar";
+import TopNavBar from "./TopNavBar";
+
 
 
 ReactDOM.render(
   <Router>
     <div>
-
+      <TopNavBar/>
       <Switch>
         <Route exact path="/" component={App}/>
         <Route exact path="/food" component={FoodScreen}/>
         <Route exact path="/exercises" component={ExercisesScreen}/>
         <Route exact path="/calories" component={CalorieTableScreen}/>
         <Route exact path="/settings" component={SettingsScreen}/>
-
       </Switch>
-
+      <NavBar/>
     </div>
   </Router>
   , document.getElementById('root')
 );
 registerServiceWorker();
+
