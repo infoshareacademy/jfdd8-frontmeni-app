fc8c5175b074890a25de1e6ebedc5cde06e2f0d8
zakrza59 <zakrza59@gmail.com>
2017-12-29 00:56:35 +0100

    Linked ProfileCreator to LoginScreen.

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -35,7 +35,7 @@ const LoginForm = () => (
           </Segment>
         </Form>
         <Message>
-          New to us? <a color='black' href='#'>Sign Up</a>
+          New to us? <a color='black' href='/profile'>Sign Up</a>
         </Message>
       </Grid.Column>
     </Grid>
