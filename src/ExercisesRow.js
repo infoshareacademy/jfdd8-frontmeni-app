83f5b4101ecedddbc49db8bde688464cb4cb308d
Dominik Dutkiewicz <dominikdut@gmail.com>
2018-01-10 14:59:28 +0100

    reformatted remove function

--- src/ExercisesRow.js
+++ src/ExercisesRow.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
+import firebase from "firebase/index";
 
 
 class ExercisesRow extends Component {
 
   handleRemoveClick = event => {
     const exerciseId = event.target.dataset.exerciseId;
-    this.props.removeExercise(exerciseId)
+    const userUid = firebase.auth().currentUser.uid;
+    firebase.database().ref(`/exercises/${userUid}/${exerciseId}`).set(null)
   };
 
   render() {
