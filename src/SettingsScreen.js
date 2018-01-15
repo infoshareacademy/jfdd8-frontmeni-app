c68557d6846d81c16ca406328d240ea62f90eb65
zakrza59 <zakrza59@gmail.com>
2018-01-15 16:12:15 +0100

    Added GoalsMenu

--- src/SettingsScreen.js
+++ src/SettingsScreen.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import TopNavBar from "./TopNavBar";
 import SettingsMenu from './SettingsMenu'
+import GoalsMenu from "./GoalsMenu";
 
 class SettingsScreen extends Component {
   render() {
     return (
       <div>
         <TopNavBar title="Settings"/>
-        <SettingsMenu/>
+        {/*<SettingsMenu/>*/}
+        <GoalsMenu/>
       </div>
     )
   }
